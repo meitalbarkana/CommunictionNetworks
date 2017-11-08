78eceb9845f7b9d99708494cdc6cebabd6438ca9
Meital <poochlayla@gmail.com>
2017-11-08 12:07:34 +0200

    meow

---
+++ utilities.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#define MAX_FILES_FOR_USER 15
+#defing MAX_USERS 15
+#define MAX_PASSWORD_LEN 25
+#define MAX_USERNAME_LEN 25
+#define MAX_FILE_SIZE 512//Bytes
+
+//TODO:: add a function that checks a given path
